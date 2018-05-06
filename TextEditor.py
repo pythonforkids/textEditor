5631e30734473682f7986abb36b175943f6428b2
Vlad <vlados02456@gmail.com>
2018-05-06 19:56:33 +0300

    commit to a new branch

--- TextEditor.py
+++ TextEditor.py
@@ -62,4 +62,6 @@ toolbar.pack(side = 'top', fill = 'x')
 exitButton = Button(toolbar, relief = 'flat', command = Quit)
 exitButton.pack(side = 'left', padx = '2', pady = '2')
 
+newButton = Button(toolbar)
+
 root.mainloop()
