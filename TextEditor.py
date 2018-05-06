71a562d4d5fce43bb37015691a59cca87408bc0f
Vlad <vlados02456@gmail.com>
2018-05-06 19:41:17 +0300

    Commit

--- TextEditor.py
+++ TextEditor.py
@@ -1,23 +1,32 @@
-# Add to github
 from tkinter import filedialog
 from tkinter import *
 
-def Quit(eval):
+def Quit():
 	global root
 	root.destroy()
 	
-def SaveFile(eval):
+def SaveFile():
 	fn = filedialog.asksaveasfilename(initialdir = "/", title = "Select file", filetypes = (("text files", "*.txt"), ("all files", "*.*")))
     
-def LoadFile(eval): 
+def LoadFile(): 
     fn = filedialog.Open(root, filetypes = [('*.txt files', '.txt')]).show()
 
-root = Tk()
+def FAQ():
+	fq = Toplevel(root)
+	fq.title('Справка')
 
-panelFrame = Frame(root, height = 40, bg = 'Gray')
+	label1 = Label(fq, text = 'Дзэн Питона\n\
+* Если реализацию сложно объяснить — это плохая идея.\n\
+* Ошибки никогда не должны замалчиваться.\n\
+* Явное лучше неявного.\n\n')
+	label1.pack()
+
+#Основное окно программы
+root = Tk()
+root.title('TextRedactor')
+
 textFrame = Frame(root, height = 340, width = 600)
 
-panelFrame.pack(side = 'top', fill = 'x')
 textFrame.pack(side = 'bottom', fill = 'both')
 
 textbox = Text(textFrame, font = 'Arial 14', wrap = 'word')
@@ -26,16 +35,29 @@ scrollbar = Scrollbar(textFrame)
 textbox.pack(side = 'left', fill = 'both', expand = 1)
 scrollbar.pack(side = 'right', fill = 'y')
 
-loadButton = Button(panelFrame, text = 'Load file')
-saveButton = Button(panelFrame, text = 'Save file')
-quitButton = Button(panelFrame, text = 'Quit')
+#Меню программы
+menubar = Menu(root, bd = 1)
+root.config(menu=menubar)
 
-loadButton.place(x = 10, width = 60, height = 40)
-saveButton.place(x = 80, width = 60, height = 40)
-quitButton.place(x = 150, width = 60, height = 40)
+fileMenu = Menu(menubar)
+FAQMenu = Menu(menubar)
 
-loadButton.bind('<Button-1>', LoadFile)
-saveButton.bind('<Button-1>', SaveFile)
-quitButton.bind('<Button-1>', Quit)
+fileMenu.add_command(label = 'Save File', command = SaveFile)
+fileMenu.add_command(label = 'Load File', command = LoadFile)
+fileMenu.add_separator()
+fileMenu.add_command(label = "Exit", command = Quit)
+
+FAQMenu.add_command(label = 'Documentation', command = FAQ)
+
+menubar.add_cascade(label="File", menu = fileMenu)
+menubar.add_cascade(label="Help", menu = FAQMenu)
+
+#Панель инструментов
+toolbar = Frame(root, bd = 1, relief = 'raised')
+toolbar.pack(side = 'top', fill = 'x')
+
+#eimg = PhotoImage(open('exit.png'))
+exitButton = Button(toolbar, relief = 'flat', command = Quit)
+exitButton.pack(side = 'left', padx = '2', pady = '2')
 
 root.mainloop()
